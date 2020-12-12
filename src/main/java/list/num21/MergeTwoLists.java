f780e9d530074dfbbfc4089b20c1bbf7fb537f0e
colourblack <932805847@qq.com>
2020-12-12 22:14:10 +0800

    linked list circle

---
+++ src/main/java/list/num21/MergeTwoLists.java
@@ -0,0 +1,37 @@
+package list.num21;
+
+import list.ListNode;
+
+/**
+ * Merge two sorted linked lists and return it as a new sorted list.
+ * The new list should be made by splicing together the nodes of the first two lists.
+ */
+public class MergeTwoLists {
+
+    /** 思路很简单，难点在于避免null的出现！ */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode node = new ListNode(0);
+        ListNode head = node;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                node.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                node.next = l1;
+                l1 = l1.next;
+            } else if (l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        return head.next;
+    }
+
+}
